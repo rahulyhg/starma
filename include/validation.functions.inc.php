30daf02cfa6965be42681ceb707ab5352fd41881
Josh Ellinghaus <jeemaill@gmail.com>
2013-04-04 10:53:10 -0500

    Live site imgs synced and domain vars set live
    Images download from live to local.  Constants for domain and landing
    page set to live, rather than local

--- include/validation.functions.inc.php
+++ include/validation.functions.inc.php
@@ -50,7 +50,7 @@ function valid_photo($photo_id, $user_id) {
   return mysql_num_rows($result) >= 1;
 }
 
-/*
+
 function get_domain () {
   return 'starma.com';
 }
@@ -62,10 +62,10 @@ function get_landing () {
 function do_redirect ($url) {
   header( 'Location: https://www.' . $url);
 }
-*/
+
 
 /**********************BEGIN DEV SERVER DOMAIN AND REDIRECT FUNCTIONS************************************/
-
+/*
 function get_domain () {
   return '127.0.0.1:8080/starma';
 }
@@ -77,7 +77,7 @@ function get_landing () {
 function do_redirect ($url) {
   header( 'Location: http://' . $url);
 }
-
+*/
 /**********************END DEV SERVER DOMAIN AND REDIRECT FUNCTIONS************************************/
 
 function validate_registration ($nickname, $password, $password2, $email, $email2, $year, $month, $day, $token) {
