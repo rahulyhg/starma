32b3a72cd51f28ac33115daa02c55c8614a21013
Josh Ellinghaus <jeemaill@gmail.com>
2013-04-17 18:29:00 -0500

    Another small pic filename fix

--- main.php
+++ main.php
@@ -110,7 +110,7 @@ if ($the_left=="nav1") {
   <img src="/img/sign_buttons/Starma-Astrology-Rahu-Ketu-Taurus-Scorpio-ON.png"/>
   <img src="/img/sign_buttons/Starma-Astrology-Rahu-Ketu-Virgo-Pisces-ON.png"/>
 
-  <img src="/img/Starma-Astrology-celebritiesBoxH.png"/>
+  <img src="/img/Starma-Astrology-CelebritiesBoxH.png"/>
   <img src="/img/Starma-Astrology-ProfileBoxH.png"/>
   <img src="/img/Starma-Astrology-ChartBoxH.png"/>
   <img src="/img/Starma-Astrology-CompareBoxH.png"/>   
