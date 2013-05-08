847de0fdaf7c48999c815094eeb5afceb01f4da4
Josh Ellinghaus <jeemaill@gmail.com>
2013-05-08 15:58:51 -0500

    Fixes an exploit that allowed > max pic  uploads

--- crop_photo.php
+++ crop_photo.php
@@ -11,20 +11,25 @@ if (login_check_point($type="full", $domain=$domain)) {
      $imgPath = ORIGINAL_IMAGE_PATH() . $imgName;
 
      if ($_POST["submit"] == "Crop and Set") { // IF A PHOTO HAS BEEN CROPPED AND POSTED IN
-        $x1 = $_POST["x1"];
-	$y1 = $_POST["y1"];
-	$x2 = $_POST["x2"];
-	$y2 = $_POST["y2"];
-	$w = $_POST["w"];
-	$h = $_POST["h"];
-        //echo PROFILE_IMAGE_PATH() . $imgName;
-        //die();
-        $cropped_profile = resizeCroppedImage(PROFILE_IMAGE_PATH() . $imgName, $imgPath,$w,$h,$x1,$y1,maxHeight() / $h);
-        $cropped_thumbnail = resizeCroppedImage(THUMBNAIL_IMAGE_PATH() . $imgName, $imgPath,$w,$h,$x1,$y1,maxHeight_thumb() / $h);
-        $cropped_compare = resizeCroppedImage(COMPARE_IMAGE_PATH() . $imgName, $imgPath,$w,$h,$x1,$y1,maxHeight_compare() / $h);
+        if (num_my_photos() < max_photos()) {
+          $x1 = $_POST["x1"];
+    	  $y1 = $_POST["y1"];
+	  $x2 = $_POST["x2"];
+	  $y2 = $_POST["y2"];
+	  $w = $_POST["w"];
+	  $h = $_POST["h"];
+          //echo PROFILE_IMAGE_PATH() . $imgName;
+          //die();
+          $cropped_profile = resizeCroppedImage(PROFILE_IMAGE_PATH() . $imgName, $imgPath,$w,$h,$x1,$y1,maxHeight() / $h);
+          $cropped_thumbnail = resizeCroppedImage(THUMBNAIL_IMAGE_PATH() . $imgName, $imgPath,$w,$h,$x1,$y1,maxHeight_thumb() / $h);
+          $cropped_compare = resizeCroppedImage(COMPARE_IMAGE_PATH() . $imgName, $imgPath,$w,$h,$x1,$y1,maxHeight_compare() / $h);
 
-        //associate_photo_with_user($imgName, get_my_user_id());
-        set_photo_cropped_status($_POST["imgID"], get_my_user_id(), 0); 
+          //associate_photo_with_user($imgName, get_my_user_id());
+          set_photo_cropped_status($_POST["imgID"], get_my_user_id(), 0); 
+        }
+        else {
+          $error=2;
+        }        
      }
      elseif ($_POST["submit"] == "<- Rotate") { // IF PHOTO NEEDS TO BE ROTATED LEFT
         
