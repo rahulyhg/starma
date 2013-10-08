cba999b710915f318bbb0cb343792bc3df9ad9c7
Josh Ellinghaus <jeemaill@gmail.com>
2013-10-08 17:12:41 -0500

    Fixes bug with PHPMailer declaration, I hope

--- chat/process_all.php
+++ chat/process_all.php
@@ -2,7 +2,7 @@
     session_start();
     require_once ('../include/db_connect.inc.php'); 
     require_once ("../include/functions.inc.php"); 
-    require_once ("PHPMailer_5.2.1/class.phpmailer.php");
+    require_once ("../PHPMailer_5.2.1/class.phpmailer.php");
     date_default_timezone_set('America/Chicago');
     //fwrite(fopen('debug.txt', 'a'), 'Begin Process..\r\n');
     //echo '*'. get_my_nickname() . '*';
