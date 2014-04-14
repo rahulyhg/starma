167dbb73acd8835b37a8b05c11a593c9976d90be
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-04-14 15:11:23 -0700

    styling message popup

--- js/msg_popup.js
+++ js/msg_popup.js
@@ -7,12 +7,25 @@ $(document).ready(function(){
 		return false;
 	});
 
-	$('.cancel').bind('click', function(){
-		$('.pop').slideFadeToggle();
+	$('.msg_cancel').bind('click', function(){
+		$('.pop').slideFadeToggle(function(){
+			$('#msg_sendie').val('');
+        	$('#send-message-area').show();
+        	$('#msg_sent').hide();
+        	$('#msg_sent').html('');
+        	$('#msg_label').text('New Message');
+        });
 	});
 
 	jQuery.fn.slideFadeToggle = function(easing, callback) {
     return this.animate({ opacity: 'toggle', height: 'toggle' }, "fast", easing, callback);
 	};
+
+	$('.msg_send').bind('click', function(){
+							$('#send-message-area').hide();
+             				$('#msg_sent').show();
+							$('#msg_sent').html('<div id="ajax_loader"><img src="/js/ajax_loader.gif" /><p>Sending...</p></div>');
+			
+					});
 
 });
