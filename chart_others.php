1fae28f119287fc898555558c840f6eead2b4cd3
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-05-07 13:51:36 -0700

    took out old links for compare page

--- chart_others.php
+++ chart_others.php
@@ -5,7 +5,8 @@ require_once ("header.php");
 if (login_check_point($type="full", $domain=$domain)) {
   if (isset($_GET["chart_id2"])) {
     $chart_id2 = $_GET["chart_id2"];
-  
+
+
     //Log the Action
     if (!isset($_POST["poi_id"])) {
       $log_poi_id = 1;
@@ -33,7 +34,7 @@ if (login_check_point($type="full", $domain=$domain)) {
     $section = $_GET["section"];
     }
 
-
+/*// OLD LINKS FOR CUSTOM CHART
     $show_links = false;
     if (is_freebie_chart($chart_id2)) {
       //if ( !chart_already_there("Alternate",get_user_id_from_chart_id($chart_id2)) ) {
@@ -52,7 +53,7 @@ if (login_check_point($type="full", $domain=$domain)) {
         echo '</div>';
       }
     }
-    
+*/ 
 
     
     
@@ -69,9 +70,9 @@ if (login_check_point($type="full", $domain=$domain)) {
     show_others_chart($goTo, $chart_id2, $western);
     // If Freebie Chart //////////////////
     if (is_freebie_chart($chart_id2)) { 
-      if ($western == 0) {
-        echo '<div class="profile_button custom_compare_button"><a href="?the_page=' . $the_page . '&the_left=' . $the_left . '&results_type=major&tier=2&stage=2">Compare Yourself to this Person</a></div>';
-      }
+      //if ($western == 0) {
+      //  echo '<div class="profile_button custom_compare_button"><a href="?the_page=' . $the_page . '&the_left=' . $the_left . '&results_type=major&tier=2&stage=2">Compare Yourself to this Person</a></div>';
+      //}
       log_this_action (compare_action_custom(), viewed_basic_action(), $log_poi_id, $chart_id2, $western);
     }
     //////////////////////////////////////
