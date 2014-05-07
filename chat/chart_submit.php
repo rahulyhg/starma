664f55b899378c49284b1ef999bf6ebd6b7f6850
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-05-07 13:51:47 -0700

    added ajax header

--- chat/chart_submit.php
+++ chat/chart_submit.php
@@ -1,9 +1,5 @@
 <?php
-session_start();
-    require_once ('../include/db_connect.inc.php'); 
-    require_once ("../include/functions.inc.php"); 
-    require_once ("../PHPMailer_5.2.1/class.phpmailer.php");
-    date_default_timezone_set('America/Chicago');
+require_once("ajax_header.php");
 
     $logged_in = login_check_point($type="full");
 
