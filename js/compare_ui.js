f25ac706191369a34732b6972fb93fe8001918a0
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-05-15 19:56:36 -0700

    added all support legs, and explanation ui

--- js/compare_ui.js
+++ js/compare_ui.js
@@ -1,12 +1,23 @@
 $(document).ready(function(){
 
-	//var autoHeight = [];
-	//	$('.blurb').each(function(){ 
-	//		var height = $(this).height()
-	//		autoHeight.push(height);
-	//		return autoHeight;				
-	//	});
-		//alert(autoHeight);
+	//Explanation
+		$('#explain_more').click(function(){
+			var text = $(this).text();
+			if(text == 'MORE') {
+				$(this).text('LESS');
+				$('#explanation').removeClass('explanation_less');
+				$('#explanation').addClass('explanation_more');
+				$('.exp_less').hide();
+				$('.exp_more').show();
+			}
+			else {
+				$(this).text('MORE');
+				$('#explanation').removeClass('explanation_more');
+				$('#explanation').addClass('explanation_less');
+				$('.exp_less').show();
+				$('.exp_more').hide();
+			}
+		});
 		
 	//Major
 		$('.text').click(function(){
@@ -22,7 +33,7 @@ $(document).ready(function(){
 			}
 			else {
 				$(this).children('.hide_show').text('HIDE TEXT');
-				$(this).siblings('.dynamic_blurb').show();
+				$(this).siblings('.dynamic_blurb').show().css('display','block');
 				//$(this).parent().css('height', 'auto');	
 				$(this).siblings('.dynamic_info').show();
 				$(this).siblings('.small_intro').hide();
@@ -39,30 +50,63 @@ $(document).ready(function(){
 
 
 	//Minor
-		$('.blurb_supporting').hide();
+		//$('.blurb_supporting').hide();
 
 
 		$('.to_leg1').click(function(){
 			$(this).parent().siblings('.leg1').toggle();
 			$(this).parent().siblings('.leg2').hide();
 			$(this).parent().siblings('.leg3').hide();
+			$(this).parent().siblings('.leg4').hide();
+			$(this).parent().siblings('.leg5').hide();
+			$(this).parent().siblings('.leg6').hide();
 		});
 
 		$('.to_leg2').click(function(){
 			$(this).parent().siblings('.leg2').toggle();
 			$(this).parent().siblings('.leg1').hide();
 			$(this).parent().siblings('.leg3').hide();
+			$(this).parent().siblings('.leg4').hide();
+			$(this).parent().siblings('.leg5').hide();
+			$(this).parent().siblings('.leg6').hide();
 		});
 
 		$('.to_leg3').click(function(){
 			$(this).parent().siblings('.leg3').toggle();
 			$(this).parent().siblings('.leg1').hide();
 			$(this).parent().siblings('.leg2').hide();
+			$(this).parent().siblings('.leg4').hide();
+			$(this).parent().siblings('.leg5').hide();
+			$(this).parent().siblings('.leg6').hide();
+		});
+
+		$('.to_leg4').click(function(){
+			$(this).parent().siblings('.leg4').toggle();
+			$(this).parent().siblings('.leg2').hide();
+			$(this).parent().siblings('.leg3').hide();
+			$(this).parent().siblings('.leg1').hide();
+			$(this).parent().siblings('.leg5').hide();
+			$(this).parent().siblings('.leg6').hide();
+		});
+
+		$('.to_leg5').click(function(){
+			$(this).parent().siblings('.leg5').toggle();
+			$(this).parent().siblings('.leg2').hide();
+			$(this).parent().siblings('.leg3').hide();
+			$(this).parent().siblings('.leg4').hide();
+			$(this).parent().siblings('.leg1').hide();
+			$(this).parent().siblings('.leg6').hide();
 		});
 
 
-
-
+		$('.to_leg6').click(function(){
+			$(this).parent().siblings('.leg6').toggle();
+			$(this).parent().siblings('.leg2').hide();
+			$(this).parent().siblings('.leg3').hide();
+			$(this).parent().siblings('.leg4').hide();
+			$(this).parent().siblings('.leg5').hide();
+			$(this).parent().siblings('.leg1').hide();
+		});
 
 
 
