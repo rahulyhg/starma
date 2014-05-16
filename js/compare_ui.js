ee3d8e2070a46a1f7425c315d04ec122dbc45750
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-05-16 15:17:03 -0700

    minor compare ui

--- js/compare_ui.js
+++ js/compare_ui.js
@@ -52,60 +52,128 @@ $(document).ready(function(){
 	//Minor
 		//$('.blurb_supporting').hide();
 
+		$('.pillar_icon_minor').click(function() {
+			$(this).parent().siblings().children('.pillar_icon_minor').not(this).removeClass('selected');
+			$(this).toggleClass('selected');
+		});
 
 		$('.to_leg1').click(function(){
-			$(this).parent().siblings('.leg1').toggle();
-			$(this).parent().siblings('.leg2').hide();
-			$(this).parent().siblings('.leg3').hide();
-			$(this).parent().siblings('.leg4').hide();
-			$(this).parent().siblings('.leg5').hide();
-			$(this).parent().siblings('.leg6').hide();
+			$(this).parent().parent().siblings('.right, .left').removeClass('selected');
+				if($(this).parent('.pillar_icon_minor').hasClass('selected')) {
+					$(this).parent().parent().siblings('.right').removeClass('selected');
+					$(this).parent().parent().siblings('.right').addClass('no_hover');
+				}
+				else {
+					$(this).parent().parent().siblings('.right').addClass('selected');
+					$(this).parent().parent().siblings('.right').removeClass('no_hover');
+				}
+			$(this).parent().parent().siblings('.leg1').toggle();
+			$(this).parent().parent().siblings('.leg2').hide();
+			$(this).parent().parent().siblings('.leg3').hide();
+			$(this).parent().parent().siblings('.leg4').hide();
+			$(this).parent().parent().siblings('.leg5').hide();
+			$(this).parent().parent().siblings('.leg6').hide();
 		});
 
 		$('.to_leg2').click(function(){
-			$(this).parent().siblings('.leg2').toggle();
-			$(this).parent().siblings('.leg1').hide();
-			$(this).parent().siblings('.leg3').hide();
-			$(this).parent().siblings('.leg4').hide();
-			$(this).parent().siblings('.leg5').hide();
-			$(this).parent().siblings('.leg6').hide();
+			$(this).parent().parent().siblings('.right, .left').removeClass('selected');
+				if($(this).parent('.pillar_icon_minor').hasClass('selected')) {
+					$(this).parent().parent().siblings('.right').removeClass('selected');
+					$(this).parent().parent().siblings('.right').addClass('no_hover');
+				}
+				else {
+					$(this).parent().parent().siblings('.right').addClass('selected');
+					$(this).parent().parent().siblings('.right').removeClass('no_hover');
+				}
+			//$('.no_hover').removeClass('selected');
+			//$('.right').addClass('selected');
+			$(this).parent().parent().siblings('.leg2').toggle();
+			$(this).parent().parent().siblings('.leg1').hide();
+			$(this).parent().parent().siblings('.leg3').hide();
+			$(this).parent().parent().siblings('.leg4').hide();
+			$(this).parent().parent().siblings('.leg5').hide();
+			$(this).parent().parent().siblings('.leg6').hide();
 		});
 
 		$('.to_leg3').click(function(){
-			$(this).parent().siblings('.leg3').toggle();
-			$(this).parent().siblings('.leg1').hide();
-			$(this).parent().siblings('.leg2').hide();
-			$(this).parent().siblings('.leg4').hide();
-			$(this).parent().siblings('.leg5').hide();
-			$(this).parent().siblings('.leg6').hide();
+			$(this).parent().parent().siblings('.right, .left').removeClass('selected');
+				if($(this).parent('.pillar_icon_minor').hasClass('selected')) {
+					$(this).parent().parent().siblings('.right').removeClass('selected');
+					$(this).parent().parent().siblings('.right').addClass('no_hover');
+				}
+				else {
+					$(this).parent().parent().siblings('.right').addClass('selected');
+					$(this).parent().parent().siblings('.right').removeClass('no_hover');
+				}
+			//$('.no_hover').removeClass('selected');
+			//$('.right').addClass('selected');
+			$(this).parent().parent().siblings('.leg3').toggle();
+			$(this).parent().parent().siblings('.leg1').hide();
+			$(this).parent().parent().siblings('.leg2').hide();
+			$(this).parent().parent().siblings('.leg4').hide();
+			$(this).parent().parent().siblings('.leg5').hide();
+			$(this).parent().parent().siblings('.leg6').hide();
 		});
 
 		$('.to_leg4').click(function(){
-			$(this).parent().siblings('.leg4').toggle();
-			$(this).parent().siblings('.leg2').hide();
-			$(this).parent().siblings('.leg3').hide();
-			$(this).parent().siblings('.leg1').hide();
-			$(this).parent().siblings('.leg5').hide();
-			$(this).parent().siblings('.leg6').hide();
+			$(this).parent().parent().siblings('.right, .left').removeClass('selected');
+				if($(this).parent('.pillar_icon_minor').hasClass('selected')) {
+					$(this).parent().parent().siblings('.left').removeClass('selected');
+					$(this).parent().parent().siblings('.left').addClass('no_hover');
+				}
+				else {
+					$(this).parent().parent().siblings('.left').addClass('selected');
+					$(this).parent().parent().siblings('.left').removeClass('no_hover');
+				}
+			//$('.no_hover').removeClass('selected');
+			//$('.left').addClass('selected');
+			$(this).parent().parent().siblings('.leg4').toggle();
+			$(this).parent().parent().siblings('.leg2').hide();
+			$(this).parent().parent().siblings('.leg3').hide();
+			$(this).parent().parent().siblings('.leg1').hide();
+			$(this).parent().parent().siblings('.leg5').hide();
+			$(this).parent().parent().siblings('.leg6').hide();
 		});
 
 		$('.to_leg5').click(function(){
-			$(this).parent().siblings('.leg5').toggle();
-			$(this).parent().siblings('.leg2').hide();
-			$(this).parent().siblings('.leg3').hide();
-			$(this).parent().siblings('.leg4').hide();
-			$(this).parent().siblings('.leg1').hide();
-			$(this).parent().siblings('.leg6').hide();
+			$(this).parent().parent().siblings('.right, .left').removeClass('selected');
+				if($(this).parent('.pillar_icon_minor').hasClass('selected')) {
+					$(this).parent().parent().siblings('.left').removeClass('selected');
+					$(this).parent().parent().siblings('.left').addClass('no_hover');
+				}
+				else {
+					$(this).parent().parent().siblings('.left').addClass('selected');
+					$(this).parent().parent().siblings('.left').removeClass('no_hover');
+				}
+			//$('.no_hover').removeClass('selected');
+			//$('.left').addClass('selected');
+			$(this).parent().parent().siblings('.leg5').toggle();
+			$(this).parent().parent().siblings('.leg2').hide();
+			$(this).parent().parent().siblings('.leg3').hide();
+			$(this).parent().parent().siblings('.leg4').hide();
+			$(this).parent().parent().siblings('.leg1').hide();
+			$(this).parent().parent().siblings('.leg6').hide();
 		});
 
 
 		$('.to_leg6').click(function(){
-			$(this).parent().siblings('.leg6').toggle();
-			$(this).parent().siblings('.leg2').hide();
-			$(this).parent().siblings('.leg3').hide();
-			$(this).parent().siblings('.leg4').hide();
-			$(this).parent().siblings('.leg5').hide();
-			$(this).parent().siblings('.leg1').hide();
+			$(this).parent().parent().siblings('.right, .left').removeClass('selected');
+				if($(this).parent('.pillar_icon_minor').hasClass('selected')) {
+					$(this).parent().parent().siblings('.left').removeClass('selected');
+					$(this).parent().parent().siblings('.left').addClass('no_hover');
+				}
+				else {
+					$(this).parent().parent().siblings('.left').addClass('selected');
+					$(this).parent().parent().siblings('.left').removeClass('no_hover');
+				}
+			//$('.no_hover').removeClass('selected');
+			//$('.left').addClass('selected');
+			$(this).parent().parent().siblings('.leg6').toggle();
+			$(this).parent().parent().siblings('.leg2').hide();
+			$(this).parent().parent().siblings('.leg3').hide();
+			$(this).parent().parent().siblings('.leg4').hide();
+			$(this).parent().parent().siblings('.leg5').hide();
+			$(this).parent().parent().siblings('.leg1').hide();
 		});
 
 
