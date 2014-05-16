1657e9e6a22857c7a169dfaa9780c88563ee1ff2
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-05-16 15:17:55 -0700

    validate current password

--- chat/ajax_change_password.php
+++ chat/ajax_change_password.php
@@ -8,8 +8,8 @@ if (isLoggedIn() == true) {
     
     if (!strlen($_POST['password']) < 6) {
       
-        if (changePassword(get_my_email(), $_POST['oldpassword'], $_POST['password'], $_POST['password2'])) {
-            
+        if (validate_current_password(get_my_email(), $_POST['oldpassword'])) {
+            changePassword(get_my_email(), $_POST['oldpassword'], $_POST['password'], $_POST['password2']);
             $data['pass'] = true; 
             //echo "Your password has been changed ! <br /> <a href='./index.php'>Return to homepage</a>";
         } 
