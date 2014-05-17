a8f72430e179e4a701e147cfc4d4c0c5798f0b88
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-05-16 17:00:58 -0700

    arrows to compare minor ui

--- js/compare_ui.js
+++ js/compare_ui.js
@@ -21,40 +21,34 @@ $(document).ready(function(){
 		
 	//Major
 		$('.text').click(function(){
-			//$(this).toggleClass('hide show');
 			var intro = $(this).siblings('.dynamic_info').text();
 			var text = $(this).text();		
 			if(text == 'HIDE TEXT') {
 				$(this).children('.hide_show').text('SHOW TEXT');
-				//$(this).parent().css('height', 'auto');
 				$(this).siblings('.dynamic_blurb').hide();
 				$(this).siblings('.dynamic_info').hide();
 				$(this).siblings('.small_intro').show();
 			}
 			else {
 				$(this).children('.hide_show').text('HIDE TEXT');
-				$(this).siblings('.dynamic_blurb').show().css('display','block');
-				//$(this).parent().css('height', 'auto');	
+				$(this).siblings('.dynamic_blurb').show().css('display','block');	
 				$(this).siblings('.dynamic_info').show();
 				$(this).siblings('.small_intro').hide();
 			}
 
-					//var x = 0;
-					//$('.blurb').each(function(x, autoHeight) {
-						//$(this).animate({height : autoHeight[x]}, 200);
-						//x++;
-					//});
 			
 		});
 
 
 
 	//Minor
-		//$('.blurb_supporting').hide();
+
+
 
 		$('.pillar_icon_minor').click(function() {
 			$(this).parent().siblings().children('.pillar_icon_minor').not(this).removeClass('selected');
 			$(this).toggleClass('selected');
+			$(this).siblings('.add_arrow_bottom').show().toggleClass('arrow_test');
 		});
 
 		$('.to_leg1').click(function(){
