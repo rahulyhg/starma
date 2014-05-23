32bbb324e2a2aca6ab959ba8466560cb23c5996d
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-05-23 16:44:46 -0700

    updated for errors

--- js/birth_form_ui.js
+++ js/birth_form_ui.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+	
+	//$('input[type=submit]').prop('disabled', true);
+
+	$('select[name=gender]').on('change', function() {
+		if($('select[name=gender]').val() != 'none') {
+			$('.gender_validation').hide().text('*Please select a gender').css('color','red');
+		}
+	});
 
 	$('input[name=time_unknown]').click(function(){
 		$('#hour_time').toggleClass('grayed_out');
@@ -6,5 +14,13 @@ $(document).ready(function(){
 		$('#meridiem_time').toggleClass('grayed_out');
 		$('#interval').toggleClass('grayed_out');
 	});
+
+	$('input[type=submit]').click(function(event){
+		if($('select[name=gender]').val() == 'none') {
+			event.preventDefault();
+			$('.gender_validation').show().text('*Please select a gender').css('color','red');
+		}
+	})
+
 
 });
