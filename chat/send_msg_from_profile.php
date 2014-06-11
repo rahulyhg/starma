f432401928b74779f52383c15cff74e49dcb3a60
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-06-11 15:14:35 -0700

    uncommented for live site

--- chat/send_msg_from_profile.php
+++ chat/send_msg_from_profile.php
@@ -11,20 +11,20 @@ $logged_in = login_check_point($type="full");
       if (isset($_POST["other_user_id"])) {
         $user_ids = array ($_POST["other_user_id"]);
       }
-      //elseif (isset($_POST["as_values_fav_user_id"])) {
-      //  $user_ids = explode(',', substr($_POST["as_values_fav_user_id"], 0, -1));
+      elseif (isset($_POST["as_values_fav_user_id"])) {
+        $user_ids = explode(',', substr($_POST["as_values_fav_user_id"], 0, -1));
         //print_r ($user_ids);
         //die();
-      //}
+      }
  
       foreach ($user_ids as $other_user_id) {
         //echo $other_user_id . '<br>';
         if (is_offline($other_user_id)) {
           $is_message = 1;
-          /************---Matt had to take this out for local server---ADD WHEN LIVE!!
+          
           if (get_preferences ($other_user_id, "chat_emails_flag", 1) == 1) {
-          sendNewMessageEmail(get_my_user_id(), $other_user_id, $message);
-          }**************/ 
+            sendNewMessageEmail(get_my_user_id(), $other_user_id, $message);
+          }
         }
 
         insert_msg_line (get_my_user_id(), $other_user_id, date("Y-m-d H:i:s"), $_POST["text_body"], 1, 0, $is_message=1);
