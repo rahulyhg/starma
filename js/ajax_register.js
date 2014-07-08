aa9932f98efd4521d3df7e65a3787fd6c52fbb7d
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-07-07 18:40:55 -0700

    ajax register form checking

--- js/ajax_register.js
+++ js/ajax_register.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 		timer = setTimeout(function() {
 			var username = { 'username' : $('input[name=nickname]').val()};
 
-			$.post('chat/username_test.php', username, function(data){
+			$.post('chat/register_form_fields.php', username, function(data){
 				if(data.errors) {	
 					$('#username_error').show().addClass('register_error').removeClass('check').text(data.message);
 				}
@@ -17,6 +17,58 @@ $(document).ready(function(){
 			}, 'json');
 		}, 1000);
 	});
+
+	$('input[name=email]').keyup(function(){
+		clearInterval(timer);
+		timer = setTimeout(function() {
+			var email = { 'email' : $('input[name=email]').val()};
+
+			$.post('chat/register_form_fields.php', email, function(data){
+				if(data.errors) {	
+					$('#email_error').show().addClass('register_error').removeClass('check').text(data.message);
+				}
+				if(data.success) {
+					$('#email_error').show().removeClass('register_error').addClass('check').text(data.message);
+				}
+			}, 'json');
+		}, 1000);
+	});
+
+	$('input[name=email2]').keyup(function(){
+		clearInterval(timer);
+		timer = setTimeout(function() {
+			var email2 = $('input[name=email2]').val();
+			var email = $('input[name=email]').val();
+			if (email != email2) {
+				$('#email2_error').show().addClass('register_error').removeClass('check').text('The two emails must match');
+			}
+			else {
+				$('#email2_error').show().removeClass('register_error').addClass('check').text('All Good');
+			}
+		}, 1500);
+	});
+/*
+	$('input[name=password]').focus(function(){
+		var default_text = 'Your password must be between 6 and 15 characters, and include only letters, numbers, underscores (_), hyphens (-), !, @';
+		$('#password_error').show().addClass('register_error').removeClass('check').text(default_text);
+	});
+*/
+	$('input[name=password]').keyup(function(){
+		clearInterval(timer);
+		timer = setTimeout(function() {
+			var password = { 'password' : $('input[name=password]').val()};
+			$.post('chat/register_form_fields.php', password, function(data){
+				//$('#password_error').show().addClass('register_error').removeClass('check').text(data);
+				if(data.errors) {	
+					$('#password_error').show().addClass('register_error').removeClass('check').text(data.message);
+				}
+				if(data.success) {
+					$('#password_error').show().removeClass('register_error').addClass('check').text(data.message);
+				}
+			}, 'json');
+		}, 1500);
+	});
+
 
 	$('form[name=register_form]').submit(function(event){
 
@@ -41,6 +93,9 @@ $(document).ready(function(){
 			if (data.errors) {
 				if (data.errors.agreement) {
 					$('#terms_error').show().addClass('register_error').removeClass('check').text(data.agreement);
+				}
+				if(data.errors.underage) {
+					$('#underage_error').show().addClass('register_error').removeClass('check').text(data.underage);
 				}
 			}
 
