3e188185407a91320c9c995ef3f96456a3b65796
Josh Ellinghaus <jeemaill@gmail.com>
2014-07-24 17:54:04 -0500

    Added copy back to enter_user.php

--- front_end/enter_user.php
+++ front_end/enter_user.php
@@ -3,6 +3,13 @@ require_once ("header.php");
 
 
 echo '<div id="custom_chart">';
+  //Log the Action
+  //log_this_action (compare_action_custom(), viewed_basic_action());
+  
+    echo '<div id="header_desc">';
+      echo 'Use the form below to enter the birth information of a friend or family member who\'s chart you would like to see.  You can even check compatibility between the two of you!';
+    echo '</div>';
+
 
     show_birth_info_form_custom_front_end();
   
