c96704f8c30bf73ea31a41de1028b542e2a61592
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-07-25 15:31:56 -0700

    added inbox

---
+++ front_end/inbox.php
@@ -0,0 +1,12 @@
+<?php
+require_once ("header.php");
+
+/***************** --Matt
+
+Need to move username and partial message from the last message to right of each user.  Message area smalle
+
+$last_msg = $user_msg_history[last];
+
+*/
+  
+  echo '<div id="inbox_guest" class="pop_guest_click">Image Coming Soon...</div>';
