74d67e56dfb11f9e0e56d72a1777551e9c6494e8
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-10-16 11:50:14 -0700

    sc

--- js/browser_detect.js
+++ js/browser_detect.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
         if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer, return version number
             //alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
-        	alert('Certain parts of Starma.com may not load properly on Internet Explorer.  For a more enjoyable experience we recommend using a recent version of Firefox or Chrome.  Thank you! :)');
+        	alert('Certain parts of Starma.com may not load properly on older versions of Internet Explorer.  For a more enjoyable experience we recommend using a recent version of Firefox or Chrome.  Thank you! :)');
         else                 // If another browser, return 0
             //alert('otherbrowser');
         	//alert('Certain versions of Internet Explorer will have difficulty loading portions of Starma.com.  For a more enjoyable experience we recommend using recent version of Firefox.  Thank you!');
