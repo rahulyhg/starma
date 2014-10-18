051bc7786b9822cb57ee0809d729bd56b58912c6
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-10-18 12:08:59 -0700

    mt test

--- admin/testing.php
+++ admin/testing.php
@@ -7,20 +7,20 @@ try {
     $template_name = 'message_test';
     $template_content = array(
         array(
-            'name' => 'example name',
-            'content' => 'example content'
+            'name' => 'username',
+            'content' => 'Matthew'
         )
     );
     $message = array(
         'html' => '<p>Example HTML content</p>',
         'text' => 'Example text content',
         'subject' => 'example subject',
-        'from_email' => 'message.from_email@example.com',
+        'from_email' => 'contact@starma.com',
         'from_name' => 'Example Name',
         'to' => array(
             array(
-                'email' => 'recipient.email@example.com',
-                'name' => 'Recipient Name',
+                'email' => 'mticciati@gmail.com',
+                'name' => 'Matthew',
                 'type' => 'to'
             )
         ),
@@ -34,7 +34,7 @@ try {
         'url_strip_qs' => null,
         'preserve_recipients' => null,
         'view_content_link' => null,
-        'bcc_address' => 'message.bcc_address@example.com',
+        'bcc_address' => '',
         'tracking_domain' => null,
         'signing_domain' => null,
         'return_path_domain' => null,
@@ -57,35 +57,18 @@ try {
                 )
             )
         ),
-        'tags' => array('password-resets'),
-        'subaccount' => 'customer-123',
-        'google_analytics_domains' => array('example.com'),
-        'google_analytics_campaign' => 'message.from_email@example.com',
-        'metadata' => array('website' => 'www.example.com'),
-        'recipient_metadata' => array(
-            array(
-                'rcpt' => 'recipient.email@example.com',
-                'values' => array('user_id' => 123456)
-            )
-        ),
-        'attachments' => array(
-            array(
-                'type' => 'text/plain',
-                'name' => 'myfile.txt',
-                'content' => 'ZXhhbXBsZSBmaWxl'
-            )
-        ),
-        'images' => array(
-            array(
-                'type' => 'image/png',
-                'name' => 'IMAGECID',
-                'content' => 'ZXhhbXBsZSBmaWxl'
-            )
-        )
+        'tags' => array(''),
+        //'subaccount' => 'customer-123',
+        'google_analytics_domains' => array(''),
+        'google_analytics_campaign' => '',
+        'metadata' => array('website' => ''),
+        'recipient_metadata' => array(),
+        'attachments' => array(),
+        'images' => array()
     );
     $async = false;
-    $ip_pool = 'Main Pool';
-    $send_at = 'example send_at';
+    $ip_pool = '';
+    //$send_at = 'example send_at';
     $result = $mandrill->messages->sendTemplate($template_name, $template_content, $message, $async, $ip_pool, $send_at);
     print_r($result);
     /*
