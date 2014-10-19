990bb43846888b3f315abcdac60f8e46990c0b67
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-10-19 13:30:21 -0700

    sf

--- include/mail.functions.inc.php
+++ include/mail.functions.inc.php
@@ -462,7 +462,7 @@ function sendTemplateReport ($to, $subject, $content, $from) {
     $template_name = 'report user';
     $template_content = array(
         array(
-            'name' => 'message',
+            'name' => 'report_user_message',
             'content' => $content
         )
     );
