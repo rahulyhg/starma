17f7636cae7102bbd2f106bc2d61767c2e0300ed
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-10-19 14:59:34 -0700

    mtpmsf

--- include/mail.functions.inc.php
+++ include/mail.functions.inc.php
@@ -209,13 +209,13 @@ function sendReportUserEmail($sender, $reported_user, $message) {
   }
 }
 
-function sendNewMessageEmail($sender_id, $receiver_id, $message) {
+function sendNewMessageEmail($sender_id, $reciever_id, $message) {
  
     $sender = basic_user_data($sender_id);
-    $receiver = basic_user_data($receiver_id);
+    $reciever = basic_user_data($receiver_id);
     $content = array(
                   'reciever' => $reciever['nickname'],
-                  'link' => '<a href="' . get_full_domain () . '/main.php?the_page=isel&the_left=nav1&other_user_id=' . $sender_id . '">Click Here</a>',
+                  'link' => '<a href="' . get_full_domain () . '/main.php?the_page=isel&the_left=nav1&other_user_id=' . $reciever_id . '"> Click Here</a>',
                   'sender' => $sender["nickname"]
                 );
     //$message = $receiver["nickname"] . ' - <Br><Br>' . $sender["nickname"] . ' has sent you a personal message on Starma.com.  <a href="' . get_full_domain () . '/main.php?the_page=isel&the_left=nav1&other_user_id=' . $sender_id . '">Click Here</a> to view it!';
