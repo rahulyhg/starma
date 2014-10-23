07e78795bce03bff1fdb4353f6269c74b9856ba1
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-10-23 16:27:27 -0700

    pgsf

--- include/display.functions.inc.php
+++ include/display.functions.inc.php
@@ -941,10 +941,10 @@ function show_photo_grid ($user_id) {
       echo '<tr>';
       $x2 = $x;
       echo $x2;
-      do-while ($x2 <= $total_photos) {
+      do {
         echo '<td>' . $user_photos[$x2] . '</td>';        
         $x2++;
-      }
+      } while ($x2 <= $total_photos);
       unset($x);
       unset($x2);
       echo '</tr>';
