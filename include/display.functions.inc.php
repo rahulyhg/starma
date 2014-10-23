e47ebb2d063adcc1e8ba8e32ebafe9596659a08a
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-10-23 16:23:22 -0700

    pgsf

--- include/display.functions.inc.php
+++ include/display.functions.inc.php
@@ -939,7 +939,7 @@ function show_photo_grid ($user_id) {
       echo '</tr>';
     if ($total_photos > 3) {
       echo '<tr>';
-      $x2 = $x+1;
+      $x2 = $x;
       echo $x2;
       while ($x2 <= $total_photos) {
         echo '<td>' . $user_photos[$x2] . '</td>';        
