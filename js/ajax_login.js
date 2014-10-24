dce57465ba46a623204963da6c2dca4ed5dc9cf2
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-10-24 14:32:44 -0700

    lftest

--- js/ajax_login.js
+++ js/ajax_login.js
@@ -27,42 +27,7 @@ $(document).ready(function(){
 
 	$('#login_password').on('keypress', function(event){
 		if(event.which == 13) {
-			//$('#go_bug_button').click(function(event){
-				$('#login_from_landing').submit(function(event){
-					event.preventDefault();
-
-			var data = {
-				'email'			  : $('#login_email').val(),
-				'password'  	  : $('#login_password').val(),
-				'stay_logged_in'  : $('input[name=stay_logged_in]').val()
-			};
-
-			$.ajax({
-				type      : 'POST',
-				url       : '/chat/login_form_fields.php',
-				data      : data,
-				dataType  : 'json',
-			})
-			.done(function(data) {
-				console.log(data);
-				if(data.errors) {
-					if(data.errors.email){
-						//alert(data.errors.email);
-						//$('#login_email_error').text(data.errors.email);
-						$('#login_email').css('border', '1px solid #C82923');
-					}
-					if(data.errors.password) {
-						//alert(data.errors.password);
-						//$('#login_password_error').text(data.errors.password);
-						$('#login_password').css('border', '1px solid #C82923');
-					}	
-				}
-				if (data.url) {
-					window.location.assign('/' + data.url);
-					//alert(data.url);
-				}
-			});
-		});
+			$('#go_bug_button').click(function(event){
 	//});
 			//$('#go_bug_button').trigger('touchstart');
 			//$('#go_bug_button').trigger('tap');
@@ -71,7 +36,7 @@ $(document).ready(function(){
 
 
 	$('#go_bug_button').click(function(event){
-		$('#login_from_landing').submit(function(event){
+		//$('#login_from_landing').submit(function(event){
 			event.preventDefault();
 
 			var data = {
@@ -105,7 +70,7 @@ $(document).ready(function(){
 					//alert(data.url);
 				}
 			});
-		});
+		//});
 
 	});
 
