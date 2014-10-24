a13a74650cf042b5e7d9bd43676672d8a4eea14e
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-10-24 13:30:10 -0700

    ipadlftest

--- js/ajax_login.js
+++ js/ajax_login.js
@@ -29,11 +29,12 @@ $(document).ready(function(){
 		if(event.which == 13) {
 			$('#go_bug_button').click();
 			$('#go_bug_button').trigger('touchstart');
+			$('#go_bug_button').trigger('tap');
 		}
 	});
 
 
-	$('#go_bug_button').on('click touchstart', function(event){
+	$('#go_bug_button').on('click touchstart tap', function(event){
 		//$('#login_from_landing').submit(function(event){
 			event.preventDefault();
 
