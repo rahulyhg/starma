f7e8e81d7c8c83f137a7da8c997c90a3e61f831d
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-10-24 14:34:39 -0700

    sf

--- js/ajax_login.js
+++ js/ajax_login.js
@@ -27,8 +27,8 @@ $(document).ready(function(){
 
 	$('#login_password').on('keypress', function(event){
 		if(event.which == 13) {
-			$('#go_bug_button').click(function(event){
-	//});
+			$('#go_bug_button').click();
+				//});
 			//$('#go_bug_button').trigger('touchstart');
 			//$('#go_bug_button').trigger('tap');
 		}
