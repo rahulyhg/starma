0f3525668277617d0ce75b44305122b5675bb8cd
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-11-07 13:52:22 -0800

    fb script to all pages

--- main.php
+++ main.php
@@ -86,6 +86,70 @@ if ($the_left=="nav1") {
     <![endif]-->
     
 <body id="bg_stars">
+  <script>
+    // This is called with the results from from FB.getLoginStatus().
+    function statusChangeCallback(response) {
+      console.log('statusChangeCallback');
+      console.log(response);
+      // The response object is returned with a status field that lets the
+      // app know the current login status of the person.
+      // Full docs on the response object can be found in the documentation
+      // for FB.getLoginStatus().
+      if (response.status === 'connected') {
+        // Logged into your app and Facebook.
+        testAPI();
+      } 
+      else if (response.status === 'not_authorized') {
+        // The person is logged into Facebook, but not your app.
+        //document.getElementById('status').innerHTML = 'Please log ' +
+        //  'into this app.';
+      } 
+      else {
+        // The person is not logged into Facebook, so we're not sure if
+        // they are logged into this app or not.
+        //document.getElementById('status').innerHTML = 'Please log ' +
+        // 'into Facebook.';
+      }
+    }
+
+    // This function is called when someone finishes with the Login
+    // Button.  See the onlogin handler attached to it in the sample
+    // code below.
+    function checkLoginState() {
+      FB.getLoginStatus(function(response) {
+        console.log('accessToken');
+        console.log(response.authResponse.accessToken);
+        statusChangeCallback(response);
+      });
+    }
+
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '349967198448431',
+        xfbml      : true,
+        version    : 'v2.1',
+        status     : true
+      });
+    };
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    function testAPI() {
+      console.log('Welcome!  Fetching your information.... ');
+      FB.api('/me', function(response) {
+        console.log('Successful login for: ' + response.name);
+        document.getElementById('status').innerHTML =
+          'Thanks for logging in, ' + response.name + '!';
+      });
+    }
+
+  </script>";
 
   <?php
   echo '<div id="msg_sheen" class="pop_invite">';
@@ -95,27 +159,6 @@ if ($the_left=="nav1") {
 
 
   echo '<script type="text/javascript" src="/js/pop_tuts.js"></script>';
-  if ($_GET['the_page'] == 'cosel') {
-    echo "<script>
-      window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '349967198448431',
-          xfbml      : true,
-          version    : 'v2.1',
-          status     : true
-        });
-      };
-
-      (function(d, s, id){
-         var js, fjs = d.getElementsByTagName(s)[0];
-         if (d.getElementById(id)) {return;}
-         js = d.createElement(s); js.id = id;
-         js.src = \"//connect.facebook.net/en_US/sdk.js\";
-         fjs.parentNode.insertBefore(js, fjs);
-       }(document, 'script', 'facebook-jssdk'));
-    </script>";
-  }
-
   ?>
 
 <div id="img_preloader">
