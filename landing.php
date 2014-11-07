ca72a59d1d720ab87563ae2441c2dc6f0fcb7995
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-11-07 12:35:16 -0800

    hide like button

--- landing.php
+++ landing.php
@@ -145,12 +145,7 @@ if (isLoggedIn())
   echo '<div id="planet"><img src="/img/planet.png" /></div>';
 
 
-  echo '<div
-  class="fb-like"
-  data-share="true"
-  data-width="450"
-  data-show-faces="true">
-</div>';
+  //echo '<div class="fb-like" data-share="true" data-width="450" data-show-faces="true"></div>';
 
   echo '</div>'; //close main container
 
