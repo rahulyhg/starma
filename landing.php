256cd2998a4b00e81dd4244e03ab1e3cd6cf7cae
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-11-07 12:34:03 -0800

    like button test

--- landing.php
+++ landing.php
@@ -145,6 +145,13 @@ if (isLoggedIn())
   echo '<div id="planet"><img src="/img/planet.png" /></div>';
 
 
+  echo '<div
+  class="fb-like"
+  data-share="true"
+  data-width="450"
+  data-show-faces="true">
+</div>';
+
   echo '</div>'; //close main container
 
   
