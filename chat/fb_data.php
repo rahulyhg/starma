82f7604730d705f430ac3b0ebc25adc0b95a427b
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-11-07 20:42:30 -0800

    t

--- chat/fb_data.php
+++ chat/fb_data.php
@@ -1,11 +1,14 @@
 <?php
 	require_once('ajax_header.php');
 
+	$data = array();
+
 	if (isset($_POST['fb_id'])) {
 		$_SESSION['fb_id'] = $_POST['fb_id'];
-		$fb_id = $_SESSION['fb_id'];
+		$data['fb_id'] = $_SESSION['fb_id'];
+		$data['check'] = 'got through';
 	}
-
-	echo json_encode($fb_id);
+	//10152592251101696
+	echo json_encode($data);
 
 ?>
