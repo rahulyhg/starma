2157544568e4f290b17dbdada8a605cf4470a6c0
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-11-13 13:20:41 -0800

    t

--- chat/test_fb_insert.php
+++ chat/test_fb_insert.php
@@ -4,24 +4,20 @@
 	$errors = array();
 
 	if (isset($_POST['test'])) {
-		/*
 		if (!update_my_fb_id($_SESSION['user_id'], $_SESSION['fb_id'])) {
 			$errors['update'] = 'error updating';
 		}
-		*/
-		/*
 		if (!get_fb_id($_SESSION['user_id'])) {
 			$errors['get'] = 'error getting id';
 		}
 		else {
 			$get = get_fb_id($_SESSION['user_id']);
 		}
-		*/
 		if (!empty($errors)){
 			$data['errors'] = $errors;
 		}
 		else {
-			$data['fb_id'] = $_SESSION['fb_id'];
+			$data['fb_id'] = $get;
 		}
 		echo json_encode($data);
 	}
