67f929a7eadb7448839ffd295e6b3b6037c538b6
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-11-13 13:00:32 -0800

    sf

--- chat/test_fb_insert.php
+++ chat/test_fb_insert.php
@@ -3,8 +3,8 @@
 	$data = array();
 	$errors = array();
 
-	if (isset($_POST['test']})) {
-		$data['fb_id'] = update_my_fb_id ($_SESSION['user_id'], $_SESSION['fb_id']);
+	if (isset($_POST['test'])) {
+		$data['fb_id'] = update_my_fb_id($_SESSION['user_id'], $_SESSION['fb_id']);
 		echo json_encode($data);
 	}
 
