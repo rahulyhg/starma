0d590dbf8aa0495ca7a56d809686cd1034921b44
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-11-13 20:14:10 -0800

    test login fb

--- landing.php
+++ landing.php
@@ -205,10 +205,12 @@ if (isLoggedIn())
       } 
       else if (response.status === 'not_authorized') {
         // The person is logged into Facebook, but not your app.
+        //setTimeout(fbLogin(), 1000);
       } 
       else {
         // The person is not logged into Facebook, so we're not sure if
         // they are logged into this app or not.
+        //setTimeout(fbLogin(), 1000);
       }
     }, {scope: 'public_profile,email,user_friends'});
   }
