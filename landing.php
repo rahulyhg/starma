0de66bc53a65153cc1a26a6778851401083f018a
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-11-13 20:20:16 -0800

    t

--- landing.php
+++ landing.php
@@ -166,11 +166,11 @@ if (isLoggedIn())
       status     : true
     });
 
-    /*
+    
     FB.getLoginStatus(function(response) {
       statusChangeCallback(response);
     });
-    */
+    
   };
 
   function fbSignUp () {
