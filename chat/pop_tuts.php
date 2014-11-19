964cace407c77e52b9ab53bdafbe078c0adb59a5
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-11-18 18:00:21 -0800

    added compare flag to settings and built more

--- chat/pop_tuts.php
+++ chat/pop_tuts.php
@@ -34,6 +34,36 @@
 		}
 	}
 
+	if(isset($_POST['cofc'])) {
+		
+		$cofc = $_POST['cofc'];
+		
+		if(!set_my_chart_flag($cofc)) {
+			$errors['set'] = 'Could not set the flag.';
+		}
+		else {
+			$cofv = my_chart_flag(); //VAL AFTER SETTING TO NEW VAL
+		}
+		if(!empty($errors)){
+			$data['errors'] = $errors;
+		}
+		else {
+			$data['success'] = true;
+			$data['cofv'] = $cofv;
+		}
+		
+	}
+
+	if (isset($_POST['compare_flag'])) {
+		$compare_flag = $_POST['compare_flag'];
+		if (!set_my_compare_flag($compare_flag)) {
+			$errors['compare_flag'] = 'There was an error turning off the tutorial.  Please refresh the page and try again.';
+		}
+		else {
+			$data['compare_flag'] = 'Success!';
+		}
+	}
+
 
 
 
--- js/pop_tuts.js
+++ js/pop_tuts.js
@@ -92,6 +92,58 @@ $(document).ready(function(){
 
 //COMPARE POPTUTS -------------------------------------------
 
+if ($('.compare_pop').length) {
+		$('.compare_pop').show();
+	}
+	
+	$('#cot1_done').click(function(){
+		$('#msg_sheen_cot1').fadeOut(300);
+		$('#msg_sheen_cot2').fadeIn(300);
+		$('#western_circle').fadeIn(300);
+		//$('#msg_sheen_screen_ct').fadeTo('slow', 0.45);
+	});
+
+	$('#cot2_done').click(function(){
+		$('#msg_sheen_cot2').fadeOut(300);
+		//$('#western_circle').fadeOut(300);
+		$('#msg_sheen_cot3'). fadeIn(300);
+		//$('#why_vedic_circle').fadeIn(300);
+	});
+
+	$('#cot3_done').click(function(){
+		//$('#msg_sheen_screen_ct').fadeTo('slow', 0.71);
+		$('#msg_sheen_cot3').fadeOut(300);
+		//$('#why_vedic_circle').fadeOut(300);
+		$('#msg_sheen_cot4'). fadeIn(300);
+	});
+
+	$('#cot4_done').click(function(){
+		$('#msg_sheen_cot4').fadeOut(300);
+		$('#msg_sheen_cot5').fadeIn(300);
+		//$('#poi_circle_left, #poi_circle_right').fadeIn(300);
+	});
+
+	$('#cot5_done').click(function(){
+		$('#msg_sheen_cot5').fadeOut(300);
+		//$('#poi_circle_left, #poi_circle_right').fadeOut(300);
+		$('.compare_pop').fadeOut(300);
+
+		var data = {'compare_flag' : 0};
+		$.ajax({
+			type 		:  'POST',
+			url 		:  '/chat/pop_tuts.php',
+			data 		:  data,
+			dataType 	: 'json',
+		})
+		.done(function(data){
+			if (data.errors) {
+				console.log(data.errors.compare_flag);
+			}
+			if (data.compare_flag) {
+				console.log(data.compare_flag);
+			}
+		});
+	});
 
 
 });
