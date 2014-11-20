b2251b117a18ee21d6f1099a016c000ec80a5040
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-11-20 13:20:08 -0800

    fbt

--- landing.php
+++ landing.php
@@ -71,6 +71,7 @@ if (isLoggedIn())
             .done(function(data){
               //alert(data.check);
               //console.log(data.fb_id);
+              userExistFB();
             });
       });
   }
