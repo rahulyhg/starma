4eed8f889b729b4e2d863bdc7e383e31762a68cf
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-11-24 15:16:57 -0800

    fbft

--- js/fb_calls.js
+++ js/fb_calls.js
@@ -65,7 +65,8 @@ function revokeFB() {
 			var fb_f = [];
       //var t = [];
 			//alert(data.fb_ids.length);
-      console.log('fb_ids: ' + data.fb_ids);
+      console.log('fb_ids: ');
+      console.log(data.fb_ids);
       //var done = 0;
       var x = 0;
 			for (i = 0; i < data.fb_ids.length; i++) {
