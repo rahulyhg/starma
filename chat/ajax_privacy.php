e968cb678b4fe228e95b929010c04d99f664134b
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-11-26 13:19:44 -0800

    removed pref_fb from ajax_privacy.php

--- chat/ajax_privacy.php
+++ chat/ajax_privacy.php
@@ -28,37 +28,6 @@
 			
 		}
 
-		elseif (isset($_POST['pref_fb'])) {
-			$pref_name = 'fb_connected';
-			if (!preg_match('%^[\d]{1}$%', $_POST['pref_fb'])) {
-				$errors['invald'] = 'There was an error. Please refresh and try again.';
-			}
-			else {
-				$pref_fb = $_POST['pref_fb'];
-				//$data['hlcb'] = $hlcb;
-				//$data['msg'] = $hlcb;
-			}
-			if (!set_my_preference($pref_name, $pref_fb)) {
-				$errors['set'] = 'Unable to set preference.  Please refresh and try again';
-			}
-			
-			if (!empty($errors)) {
-				$data['errors'] = $errors;
-			}				
-			else {
-				$data['success'] = true;
-				$data['msg'] = 'Success!';
-				if (get_my_preferences($pref_name, 0) == 0) {
-					$data['unset'] = true;
-				}
-				else {
-					$data['set'] = true;
-				}
-			}
-				//$data['msg'] = is_preference_there($pref_name, get_my_user_id());
-			
-		}
-
 		echo json_encode($data);
 	}
 	else {
