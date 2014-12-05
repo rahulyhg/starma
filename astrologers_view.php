d243598bfbaa0fd43fbd4ada94f6d90e6473dfdd
Josh Ellinghaus <jeemaill@gmail.com>
2014-12-05 16:55:58 -0600

    Added Chart Privacy Checks to the Profile Page
    - Profile
    - House Lords
    - Astrology's View

--- astrologers_view.php
+++ astrologers_view.php
@@ -1,6 +1,6 @@
 <?php
 	require ('header.php');
-	if (isLoggedIn()) {
+if (isLoggedIn()) {
 
 		$western = $_GET['western'];
 
@@ -19,6 +19,7 @@
 		else {  //MY PROFILE
 			$chart_id = get_my_chart_id();
 		}
+
 		
     	if ($_GET['the_page'] == 'psel') { //MY PROFILE
     		//if ($western == 0) {
@@ -40,12 +41,22 @@
 
     	}
     	else { //ANOTHER PROFILE
-        echo '<div id="av_type" class="later_on">';
+           $pref = get_preferences(get_user_id_from_chart_id($chart_id), 'chart_private', 0);
+           $my_pref = get_my_preferences('chart_private', 0);
+           if ($pref == 1 && !isCeleb(get_user_id_from_chart_id($chart_id))) {
+             $username = get_nickname(get_user_id_from_chart_id($chart_id));
+             echo '<div class="later_on" style="font-size:1.5em; text-align:center; margin-bottom:260px;">' . $username . ' has chosen to keep this section private.</div>';
+           }
+           elseif ($my_pref == 1 && !isCeleb(get_user_id_from_chart_id($chart_id))) {
+              echo '<div class="later_on" style="font-size:1.5em; text-align:center; margin-bottom:260px;">You cannot view this section of another person\'s profile while your chart info is set as private.  <a href="/main.php?the_page=ssel&the_left=nav1&the_tier=1">View My Privacy Settings</a></div>';
+           }
+           else {
+              echo '<div id="av_type" class="later_on">';
     		if (!$western_there) {
     			if ($isCeleb) {
               $user_info = profile_info(get_user_id_from_chart_id($chart_id));
               $username = $user_info['first_name'] . ' ' . $user_info['last_name'];
-          }
+               }
     			else {
 					  $username = get_nickname(get_user_id_from_chart_id($chart_id));
     			}
@@ -53,27 +64,29 @@
             echo '<span>' . $username . ' only has a Vedic Birth Chart</span>';
     		}
     		else {  
-          echo '<a class="later_on" ';
-            if ($western == 0) {
-              echo 'style="text-decoration:underline;"';
-            }
-          echo ' href="?the_page=' . $the_page .'&the_left=' . $the_left . '&chart_id2=' . $chart_id . '&tier=' . $tier . '&western=0&section=astrologers_view_selected">Vedic</a>  |  <span>Western (Coming Soon)</span>';
+            echo '<a class="later_on" ';
+              if ($western == 0) {
+                echo 'style="text-decoration:underline;"';
+              }
+            echo ' href="?the_page=' . $the_page .'&the_left=' . $the_left . '&chart_id2=' . $chart_id . '&tier=' . $tier . '&western=0&section=astrologers_view_selected">Vedic</a>  |  <span>Western (Coming Soon)</span>';
           
-          /*//IMPLEMENT WHEN WESTERN VIEW EXISTS
-          echo '<a class="later_on"';  
-            if ($western == 1) {
-              echo 'style="text-decoration:underline;"';
-            }
-          echo ' href="?the_page=' . $the_page .'&the_left=' . $the_left . '&chart_id2=' . $chart_id . '&tier=' . $tier . '&western=1&section=astrologers_view_selected">Western (Coming Soon)</a>';
-          */
+            /*//IMPLEMENT WHEN WESTERN VIEW EXISTS
+            echo '<a class="later_on"';  
+              if ($western == 1) {
+                echo 'style="text-decoration:underline;"';
+              }
+            echo ' href="?the_page=' . $the_page .'&the_left=' . $the_left . '&chart_id2=' . $chart_id . '&tier=' . $tier . '&western=1&section=astrologers_view_selected">Western (Coming Soon)</a>';
+            */
+           }
         }        
         echo '</div>';
     	}
     	
-			
+	if ($pref == 0 && $my_pref == 0 && !isCeleb(get_user_id_from_chart_id($chart_id))) {		
 		show_astrologers_view();
+        }
 		
-	}
+}
 
 	
 
--- chart_others.php
+++ chart_others.php
@@ -3,59 +3,69 @@ require_once ("header.php");
 
   
 if (login_check_point($type="full", $domain=$domain)) {
+  
   if (isset($_GET["chart_id2"])) {
     $chart_id2 = $_GET["chart_id2"];
-
-
     //Log the Action
-    if (!isset($_POST["poi_id"])) {
-      $log_poi_id = 1;
+    $pref = get_preferences(get_user_id_from_chart_id($chart_id2), 'chart_private', 0);
+    $my_pref = get_my_preferences('chart_private', 0);
+    if ($pref == 1 && !isCeleb(get_user_id_from_chart_id($chart_id2))) {
+      $username = get_nickname(get_user_id_from_chart_id($chart_id2));
+      echo '<div class="later_on" style="font-size:1.5em; text-align:center; margin-bottom:260px;">' . $username . ' has chosen to keep this section private.</div>';
+    }
+    elseif ($my_pref == 1 && !isCeleb(get_user_id_from_chart_id($chart_id2))) {
+      echo '<div class="later_on" style="font-size:1.5em; text-align:center; margin-bottom:260px;">You cannot view this section of another person\'s profile while your chart info is set as private.  <a href="/main.php?the_page=ssel&the_left=nav1&the_tier=1">View My Privacy Settings</a></div>';
     }
     else {
-      $log_poi_id = $_POST["poi_id"];
-    }
-
-    if (!isset($_GET["western"])) {
-      $western = 0;
-    }
-    else {
-      $western = $_GET["western"];
-    }
-   
-    if (is_freebie_chart($chart_id2)) {  
-      if ($western == 1) {
       
-        $chart_id2 = chart_already_there("Alternate_Freebie1",get_my_user_id());
+      if (!isset($_POST["poi_id"])) {
+        $log_poi_id = 1;
       }
       else {
-        $chart_id2 = chart_already_there("Freebie1",get_my_user_id());
+        $log_poi_id = $_POST["poi_id"];
       }
 
-    $section = $_GET["section"];
-    }
+      if (!isset($_GET["western"])) {
+        $western = 0;
+      }
+      else {
+        $western = $_GET["western"];
+      }
+   
+      if (is_freebie_chart($chart_id2)) {  
+        if ($western == 1) {
+        
+          $chart_id2 = chart_already_there("Alternate_Freebie1",get_my_user_id());
+        }
+        else {
+          $chart_id2 = chart_already_there("Freebie1",get_my_user_id());
+        }
+
+      $section = $_GET["section"];
+      }
 
    
 
-/*// OLD LINKS FOR CUSTOM CHART
-    $show_links = false;
-    if (is_freebie_chart($chart_id2)) {
-      //if ( !chart_already_there("Alternate",get_user_id_from_chart_id($chart_id2)) ) {
-        $show_links = true;
-      //}
-    }
-    if ($show_links) {
-      if ($western == 0) {
-        echo '<div id="chart_direction_link_west">';
-          echo '<a href="?the_page=' . $the_page . '&the_left=' . $the_left . '&tier=4&chart_id2=' . $chart_id2 . '&western=1&section=' . $section . '">Starma Western Chart --></a>';
-        echo '</div>';
+  /*// OLD LINKS FOR CUSTOM CHART
+      $show_links = false;
+      if (is_freebie_chart($chart_id2)) {
+        //if ( !chart_already_there("Alternate",get_user_id_from_chart_id($chart_id2)) ) {
+          $show_links = true;
+        //}
       }
-      else {
-        echo '<div id="chart_direction_link_east">';
-          echo '<a href="?the_page=' . $the_page . '&the_left=' . $the_left . '&tier=4&chart_id2=' . $chart_id2 . '&western=0section=' . $section . '"><-- Starma Eastern Chart</a>';
-        echo '</div>';
+      if ($show_links) {
+        if ($western == 0) {
+          echo '<div id="chart_direction_link_west">';
+            echo '<a href="?the_page=' . $the_page . '&the_left=' . $the_left . '&tier=4&chart_id2=' . $chart_id2 . '&western=1&section=' . $section . '">Starma Western Chart --></a>';
+          echo '</div>';
+        }
+        else {
+          echo '<div id="chart_direction_link_east">';
+            echo '<a href="?the_page=' . $the_page . '&the_left=' . $the_left . '&tier=4&chart_id2=' . $chart_id2 . '&western=0section=' . $section . '"><-- Starma Eastern Chart</a>';
+          echo '</div>';
+        }
       }
-    }
-*/ 
+  */ 
 
     
     
@@ -63,25 +73,28 @@ if (login_check_point($type="full", $domain=$domain)) {
       
     
   
-    if (!is_freebie_chart($chart_id2)) {
-      $goTo="?the_page=" . $the_page . "&the_left=" . $the_left . "&tier=3&western=" . $western . "&section=" . $section; 
-    }
-    else {
-      $goTo="?the_page=" . $the_page . "&the_left=" . $the_left . "&tier=4&western=" . $western . "&section=" . $section; 
-    }
-    show_others_chart($the_page, $the_left, $chart_id2, $western);
-    // If Freebie Chart //////////////////
-    if (is_freebie_chart($chart_id2)) { 
-      //if ($western == 0) {
-      //  echo '<div class="profile_button custom_compare_button"><a href="?the_page=' . $the_page . '&the_left=' . $the_left . '&results_type=major&tier=2&stage=2">Compare Yourself to this Person</a></div>';
-      //}
-      log_this_action (compare_action_custom(), viewed_basic_action(), $log_poi_id, $chart_id2, $western);
-    }
-    //////////////////////////////////////
-    else {
-       log_this_action (compare_action_chart(), viewed_basic_action(), $log_poi_id, $chart_id2, $western, get_user_id_from_chart_id($chart_id2));
-    }
+      if (!is_freebie_chart($chart_id2)) {
+        $goTo="?the_page=" . $the_page . "&the_left=" . $the_left . "&tier=3&western=" . $western . "&section=" . $section; 
+      }
+      else {
+        $goTo="?the_page=" . $the_page . "&the_left=" . $the_left . "&tier=4&western=" . $western . "&section=" . $section; 
+      }
+      show_others_chart($the_page, $the_left, $chart_id2, $western);
+      // If Freebie Chart //////////////////
+      if (is_freebie_chart($chart_id2)) { 
+        //if ($western == 0) {
+        //  echo '<div class="profile_button custom_compare_button"><a href="?the_page=' . $the_page . '&the_left=' . $the_left . '&results_type=major&tier=2&stage=2">Compare Yourself to this Person</a></div>';
+        //}
+        log_this_action (compare_action_custom(), viewed_basic_action(), $log_poi_id, $chart_id2, $western);
+      }
+      //////////////////////////////////////
+      else {
+         log_this_action (compare_action_chart(), viewed_basic_action(), $log_poi_id, $chart_id2, $western, get_user_id_from_chart_id($chart_id2));
+      }
+    } 
+    
   }
+   
   else {
     echo 'No Chart Found';
   }
