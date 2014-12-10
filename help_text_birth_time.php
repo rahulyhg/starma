4f4e30f568b6c0ae5513d7ed9d2a02ea09b8b30e
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-12-09 23:06:49 -0800

    New text for birth time help

--- help_text_birth_time.php
+++ help_text_birth_time.php
@@ -1,25 +1,24 @@
 <?php require_once ("header.php"); ?>
 
 <body>
-<div class="help_text">
-    <span style="font-size:2em">Finding My Time of Birth</span>
-    <br><br>
-    Below are instructions on how to find your time of birth.  If you'd like skip this step for now, close this window, click the "Done" button and we'll remind you later.
-    <br><br>
-    The first place to check is your “long form” birth certificate.  The “short form” does not include your birth time.  If you don’t have your long form birth certificate, there is a good chance one of your parents or a legal guardian does!  Ask your mother, father, or legal guardian and see if they have a record of your birth time.  If they don’t have it, ask them if they recorded your birth time in a baby book, family bible, or journal.  
-    <br><br>
-    We highly recommend that you DON’T guess.  If your parents or legal guardians are “pretty sure” you were born “around 6am”, please be sure to find out how far off they might be.  Once you have a good idea of how accurate or inaccurate the estimate might be, please use our “Accuracy of Time” function so that we can determine how much your astrological birth chart may have changed during the window of time you provide to us.  This way we can provide you with as many accurate astrological placements as possible.
-    <br><br>
-    If you can’t find your birth certificate and your parents or legal guardian(s) don’t have any record of your birth time don’t despair!  There are several other options.
-    <br><br>
-    The county in which you were born will have an “Office of Vital Statistics”, “Department of Vital Records”, “Birth and Death Records”, or similar office.  By performing an Internet search for one of these offices in connection with your birth location you should be able to find the appropriate office. (e.g. Google search: office of vital statistics Fairfield Iowa).  The county office should have your birth certificate on file.
-    <br><br>
-    If you want to go the commercial route, there are businesses that will hunt down your birth certificate for you such as www.vitalchek.com*.  If you use a company like this make sure to order your full birth certificate sometimes referred to as “Birth Long”, or “Birth Full”.  
-    <br><br>
-    Contacting the hospital where you were born is an option.  However, they send their records to the local office that stores birth records, so they may not keep birth records themselves.  
-    <br><br>
-    If you were born at home, ask your parents or a legal guardian for the name of the midwife who delivered you.  Midwives fill out a birth certificate form and send it to the local office that stores birth records just like hospitals do.  However, they may keep a log of births they have been involved in which may include your birth time.
-    <br><br>
-    * Starma LLC does not endorse, nor guarantee usefulness or quality of any outside business.
- </div> 
+
+    <div class="later_on" style="font-size:2em">Options for Finding Your Time of Birth</div>
+    <div class="help_text">
+        <br><br>
+        <strong>1. LONG FORM BIRTH CERTIFICATE</strong>  -  A long form birth certificate often includes your time of birth (the "short form" does not).  Look through your important documents file.  If you don't have it...
+        <br><br>
+        <strong>2. ASK YOUR PARENTS</strong> -  There's a good chance one of them held onto this precious document.  If they can't find it, ask them if they recorded your birth time in a baby book, family journal, bible or religious text.  If they have no clue...  
+        <br><br>
+        <strong>3. ASK THE COUNTY</strong>  -  The county in which you were born will have an office with a title like the following: "Office of Vital Statistics," "Department of Vital Records," "Birth and Death Records," etc.  Do a google search for the county in which you were born with one of these names.  For example, "Office of Vital Statistics San Francisco, CA"  Once you have their contact info, give them a call and request your long form birth certificate.  It's an important document to have anyway.  If this process is too much of a hassle...
+        <br><br>
+        <strong>4. HIRE A PRO TO FIND IT</strong>  -  Companies like VitalChek* ( <a href="http://www.vitalchek.com" title="VitalChek">www.vitalchek.com</a> ) will hunt down your birth certificate for you.  Make sure you tell them you need your LONG FORM birth certificate.  This is sometimes referred to as the "Birth Long" or "Birth Full."
+        <br><br>
+        <strong>5. CONTACT THE HOSPITAL WHERE YOU WERE BORN</strong>  -  The hospitals sometimes keep birth records.  Even if they don't, they'll likely know what county office they send their birth records to.
+        <br><br>
+        <strong>6. HOME BIRTH? CALL THE MIDWIFE</strong>  -  Some midwives keep logs of the births they attended, and in some cases, they include time of birth.  Even if the midwife doesn't keep a log, she filled out your birth certificate, so she'll likely know what county office she sent your birth certificate to.
+        <br><br>
+        <strong>7. DON'T GUESS</strong>  -  If you have an idea of what time you were born, for example "between 4am and 6am," select "within an hour" of "5am" from the "Accuracy" drop down.  Sometimes, this is a small enough window of time for us to determine your Birth Chart.   
+        <br><br>
+        <span style="font-size:.7em;">* Starma LLC does not endorse, nor guarantee usefulness or quality of any outside business.</span>
+    </div> 
 </body>
