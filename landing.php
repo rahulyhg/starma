9e718b0dcfe14e86b0da0b262b3c92fbba0c876a
starmallc <mticciati@matt-ticciatis-macbook.local>
2014-12-11 18:11:54 -0800

    about on landing goes to guest about starma

--- landing.php
+++ landing.php
@@ -295,7 +295,7 @@ if (isLoggedIn())
           <div id="footer_links">
             <ul>
               <li><a class="padding_right">Starma LLC 2014</a></li>
-              <li><a class="padding_right" title="Coming soon...">About Starma</a></li>
+              <li><a class="padding_right" href="' . get_full_domain() . '/guest/main.php?the_left=nav2&the_page=hsel" title="About Starma">About Starma</a></li>
               <li><a class="padding_right" href="docs/privacyPolicy.htm" target="_blank">Privacy</a></li>
               <li><a class="padding_right" href="docs/termsOfUse.htm" target="_blank">Terms</a></li>
               <li><a href="mailto:contact@starma.com" title="contact@starma.com">Contact</a></li>
