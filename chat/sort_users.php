2d34fa44757673e366ef64926618df62109e6fce
starmallc <mticciati@matt-ticciatis-macbook.local>
2015-01-09 21:56:23 -0800

    sf

--- chat/sort_users.php
+++ chat/sort_users.php
@@ -6,20 +6,20 @@
 		$data = array();
 		$errors = array();
 		$sign_id = $_POST['sign_id'];
-		if (!preg_match('%[0-9]%', $sign_id)) {
+		if (!preg_match('%^[0-9]$%', $sign_id)) {
 			$errors['valid'] = 'something is wrong with this sign...';
 		}
 		if (!empty($errors)) {
 			$data['errors'] = $errors;
 		}
 		else {
-			$data['sign'] = get_sign_name($sign_id);
+			$data['sign_id'] = get_sign_name($sign_id);
 			//$data['sign'] = $sign_id
 		}
 		
 		//$data = get_sign_name(4);
 		echo json_encode($data);
-	//}
+	}
 
 
 
