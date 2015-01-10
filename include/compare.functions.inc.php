1915a663e5f6ae800141c718b699e4300c0048d0
starmallc <mticciati@matt-ticciatis-macbook.local>
2015-01-10 15:08:12 -0800

    tsm

--- include/compare.functions.inc.php
+++ include/compare.functions.inc.php
@@ -65,8 +65,8 @@ function get_single_suggested_match($user_id) {
 }
 
 function get_random_charts() {
-  $q = 'SELECT chart_id from chart, user.nickname from user
-  right join user on chart.user_id = user.user_id 
+  $q = 'SELECT chart_id from chart
+  inner join user on chart.user_id = user.user_id 
   where method = "E" and chart.nickname = "Main" and interval_time = 0 AND NOT user.nickname like "testceleb%"
   ORDER BY RAND()
   LIMIT 20';
