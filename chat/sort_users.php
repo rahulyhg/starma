9ff3572caa48e15c48f583b983243143b960d81d
starmallc <mticciati@matt-ticciatis-macbook.local>
2015-01-09 21:37:46 -0800

    t

--- chat/sort_users.php
+++ chat/sort_users.php
@@ -1,20 +1,23 @@
 <?php
 	require_once('ajax_header.php');
 
+	/*
 	if (isset($_POST['sort_by_sign'])) {
 		$data = array();
 		$errors = array();
 		$sign_id = $_POST['sign_id']
-		//if (!preg_match('%[1-12]%', $sign_id)) {
-		//	$errors['valid'] = 'something is wrong with this sign...';
-		//}
+		if (!preg_match('%[1-12]%', $sign_id)) {
+			$errors['valid'] = 'something is wrong with this sign...';
+		}
 		if (!empty($errors)) {
 			$data['errors'] = $errors;
 		}
 		else {
-			//$data['sign'] = get_sign_name($sign_id);
-			$data['sign'] = $sign_id
+			$data['sign'] = get_sign_name($sign_id);
+			//$data['sign'] = $sign_id
 		}
+		*/
+		$data = 'hello';
 		echo json_encode($data);
 	}
 
