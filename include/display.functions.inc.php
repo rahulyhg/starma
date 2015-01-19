6e4bda255f7238feb9f54a7e0b190bcb04ea6b54
starmallc <mticciati@matt-ticciatis-macbook.local>
2015-01-19 15:56:27 -0800

    sf

--- include/display.functions.inc.php
+++ include/display.functions.inc.php
@@ -7675,7 +7675,7 @@ function show_sign_up_or_sign_in_box () {
         echo '<button type="button" id="sign_in_button" class="sign_up">Log In</button>';
       echo '</div>';
 
-      echo '<button type="button" class="sign_up" onClick="window.location=/guest/main.php;">Explore Starma</button>';
+      echo '<button type="button" class="sign_up" onClick="window.location.assign(/guest/main.php);">Explore Starma</button>';
 
     echo '</div>';
   echo '</div>';
